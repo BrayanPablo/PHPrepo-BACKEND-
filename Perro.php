57e117563edaccae09b3415daa326feb510882c9
Brayan pablo <1518925enati.pe>
2024-05-14 10:02:01 -0500

    Se actualizao el proyecto y se controlo los datos ingresados

--- Perro.php
+++ Perro.php
@@ -1,13 +1,26 @@
 <?php
 class Perro{
     // Propiedades
-    private int $peso = 0;
+    private int $peso = 0 ;
     private string $raza = 'sin raza';
     private string $color = 'sin color';
     private string $nombre = 'sin nombre';
 
-    // metodos 
+    // métodos 
     public function getpropiedades():string{
         return "$this->raza,$this->peso,$this->color,$this->nombre";
     }
-}
+    //Establecer las condiciones para que el valor ingresado solo contenga letras y que su longitud
+    //no supere los 20 caracteres y que devuelva un booleano si la operación fue exitosa o no.
+    public function setNombre(string $valor):bool{
+        $mensaje_error = TRUE;
+        (ctype_alpha($valor)&& strlen($valor)<=20) ? $this->nombre=$valor : $mensaje_error = FALSE;
+        return  $mensaje_error;
+    } 
+    //metodo para controlar el peso del perro par aque acepte solo numeros y el peso este entre 0 a 60
+    public function setPeso(int $dato):bool{
+        $mensaje_error = TRUE;
+        ctype_digit($dato) or ($dato > 0 && $dato<=60) ?$this->peso=$dato :$mensaje_error = FALSE;
+        return $mensaje_error;
+        }
+    }
--- index.php
+++ index.php
@@ -2,11 +2,20 @@
 declare(strict_types=1);
 include('Perro.php');
 $perro1 = new Perro;
-$propiedades_perro = $perro1->getpropiedades();
-list($raza, $peso, $color, $nombre) = explode(',',$propiedades_perro);
-
 
 
 //Imprimiendo datos 
-echo "La raza del perro es $raza, su peso es $peso, su color es $color y finalmente su nombre es $nombre";
-    
+//echo "La raza del perro es $raza, su peso es $peso, su color es $color y finalmente su nombre es $nombre";
+$errorPeso = $perro1->setPeso(-10);
+$errorNombre = $perro1->setNombre('firulais');
+echo $errorNombre ? "Nombre del perro atualizado " :'Error al actualizar el nombre';
+echo $errorPeso ? "peso del perro atualizado " :'Error al actualizar el peso';
+$propiedades_perro = $perro1->getpropiedades();
+list($raza, $peso, $color, $nombre) = explode(',',$propiedades_perro);
+echo "<br> Nombre: <b>". $nombre."</b>";
+echo "<br> Peso: <b>". $peso."</b>";
+echo "<br> Color: <b>". $color."</b>";
+echo "<br> Raza: <b>". $raza."</b>";
+
+
+
